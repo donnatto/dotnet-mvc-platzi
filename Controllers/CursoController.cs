b73344afac479fc9d4c14401b381a07413e7b484
Edwin Dominguez <edwindominguez@hotmail.com>
2020-01-30 15:27:48 -0500

    add create curso form

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -32,6 +32,27 @@ namespace Platzi_MVC.Controllers
             return View("MultiCurso", _context.Cursos);
         }
 
+        public IActionResult Create()
+        {
+            ViewBag.Fecha = DateTime.Now;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Curso curso)
+        {
+            ViewBag.Fecha = DateTime.Now;
+
+            var escuela = _context.Escuelas.FirstOrDefault();
+
+            curso.EscuelaId = escuela.Id;
+            _context.Cursos.Add(curso);
+            _context.SaveChanges();
+
+            return View();
+        }
+
         private EscuelaContext _context;
         public CursoController(EscuelaContext context)
         {
